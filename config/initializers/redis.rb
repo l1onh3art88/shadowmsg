899442f1aa342c0aaa7e04a0147fc25d9c4cbfc5
vagrant <vagrant@rails-dev-box>
2014-08-11 21:37:39 +0000

    reconfigured redis.rb

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,2 +1,2 @@
-redis_conf = YAML::load(File.read(Rails.root.join("config", "redis.yml")))[Rails.env]
-REDIS = Redis.new(redis_conf)
+uri = URI.parse(ENV["REDISTOGO_URL"])
+REDIS = Redis.new(:host => hoki.redistogo.com, :port => 9911, :password => 23d7d203566bda6a96f1a31cbe3d9f1f)
