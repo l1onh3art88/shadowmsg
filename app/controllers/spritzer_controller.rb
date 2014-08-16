d2e1a58a4f84141d007ecd34db3882ef353fc9a6
vagrant <vagrant@rails-dev-box>
2014-08-16 17:33:51 +0000

    spritzcontroller 2.o

--- app/controllers/spritzer_controller.rb
+++ app/controllers/spritzer_controller.rb
@@ -1,4 +1,16 @@
 class SpritzerController < ApplicationController
+
+
+
+	<script type="text/javascript">
+	     var SpritzSettings = {
+	               clientId: "6c3a147d2cd7d078b",
+	               redirectUri:"http://www.shadowmsg.com/messages/login_success"
+	     };
+	</script> 
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+	<script type="text/javascript" src="//sdk.spritzinc.com/js/1.2/js/spritz.min.js"></script>
+	
 	var onStartSpritzClick = function(event) {
     var text = $('#inputText').val();
     var locale = "en_us;";
